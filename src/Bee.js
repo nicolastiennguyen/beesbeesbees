4e8655ecbd424c7b261828ebc574c8e81d2e6fc8
Nick <nicolastiennguyen@gmail.com>
2022-07-25 11:06:32 -0700

    successfully implement Bee class in Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  // calling the superclass allows this new subclass to inherit its properties
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+// reset Subclass.prototype & Subclass.prototype.constructors
+
+// setting the prototype relationship between the subclass instantiation and the superclass
+Bee.prototype = Object.create(Grub.prototype);
+// the previous line overwrote the prototype constructor in this class
+// so we must setup our subclass relationship to its Constructor prototype object
+Bee.prototype.constructor = Bee;
